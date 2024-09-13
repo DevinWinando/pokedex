c06759e7694254f87e8a3f249a932c9ceeb7cb83
DevinWinando <devin.winando13@gmail.com>
2024-09-13 16:47:25 +0700

    fix: className warning

--- pokedex-fe/pages/index.js
+++ pokedex-fe/pages/index.js
@@ -11,14 +11,14 @@ export default function index() {
         <div className={`flex flex-wrap justify-center mt-8 gap-4`}>
           <Link href={"/pokemons"}>
             <div className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 ">
-              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
                 Search Pokemon!
               </h5>
             </div>
           </Link>
           <Link href={"my-pokemons"}>
             <div className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 ">
-              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
                 My Pokemon!
               </h5>
             </div>
